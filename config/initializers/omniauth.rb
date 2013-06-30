ca76f049549a95844ab734c8cfd8c43e243fafb2
Enrique Martín Martínez <enriquemartinmartinez@gmail.com>
2013-06-30 09:56:38 -0700

    Updated omniauth api key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '454168980577', 'QNuBPc6b5Sh-ew_2Nh0u6ZWz',
+  #provider :google_oauth2, '454168980577', 'QNuBPc6b5Sh-ew_2Nh0u6ZWz',
+  provider :google_oauth2, '282259666783', 'Q7BHN-SDm5m1Buck_S_01QCq',
   	{
     	:scope => "userinfo.email,userinfo.profile,plus.me",
     	:force_login => true,
